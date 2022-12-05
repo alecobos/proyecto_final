d63a539124772dfb246ce69d77ad648831607dc1
alecobos <alecobos@gmail.com>
2022-12-05 14:14:20 -0300

    .

--- frmlogin.py
+++ frmlogin.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 import tkinter.font as tkFont
 import tkinter.messagebox as tkMsgBox
-#from frmuser import User
-#from frmdashboard import Dashboard
-#import bll.usuarios as user
+from frmuser import User
+from frmdashboard import Dashboard
+import bll.usuarios as user
 
 class Login(tk.Toplevel):
     def __init__(self, master=None):
@@ -15,8 +15,25 @@ class Login(tk.Toplevel):
         screenwidth = master.winfo_screenwidth()
         screenheight = master.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        master.geometry(alignstr)
-        master.resizable(width=False, height=False)
+        self.geometry(alignstr)
+        self.resizable(width=False, height=False)
+        GLineEdit_511=tk.Entry(self, name="txtUsuario")
+        GLineEdit_511["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        GLineEdit_511["font"] = ft
+        GLineEdit_511["fg"] = "#333333"
+        GLineEdit_511["justify"] = "left"
+        GLineEdit_511["text"] = ""
+        GLineEdit_511.place(x=110,y=10,width=280,height=25)
+
+        GLineEdit_723=tk.Entry(self, name ="txtContrasenia", show="*")
+        GLineEdit_723["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        GLineEdit_723["font"] = ft
+        GLineEdit_723["fg"] = "#333333"
+        GLineEdit_723["justify"] = "left"
+        GLineEdit_723["text"] = ""
+        GLineEdit_723.place(x=110,y=50,width=280,height=25)
 
         GLabel_787=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
@@ -33,25 +50,7 @@ class Login(tk.Toplevel):
         GLabel_267["justify"] = "center"
         GLabel_267["text"] = "Contraseña:"
         GLabel_267.place(x=30,y=50,width=70,height=25)
-
-        GLineEdit_511=tk.Entry(self)
-        GLineEdit_511["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_511["font"] = ft
-        GLineEdit_511["fg"] = "#333333"
-        GLineEdit_511["justify"] = "left"
-        GLineEdit_511["text"] = ""
-        GLineEdit_511.place(x=110,y=10,width=280,height=30)
-
-        GLineEdit_723=tk.Entry(self)
-        GLineEdit_723["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_723["font"] = ft
-        GLineEdit_723["fg"] = "#333333"
-        GLineEdit_723["justify"] = "left"
-        GLineEdit_723["text"] = ""
-        GLineEdit_723.place(x=110,y=50,width=280,height=30)
-        GLineEdit_723["show"] = ""*""
+        
 
         GButton_962=tk.Button(self)
         GButton_962["bg"] = "#f0f0f0"
@@ -61,7 +60,7 @@ class Login(tk.Toplevel):
         GButton_962["justify"] = "center"
         GButton_962["text"] = "Aceptar"
         GButton_962.place(x=230,y=100,width=70,height=25)
-        GButton_962["command"] = self.GButton_962_command
+        GButton_962["command"] = self.iniciar_sesion
 
         GButton_290=tk.Button(self)
         GButton_290["bg"] = "#f0f0f0"
@@ -71,7 +70,7 @@ class Login(tk.Toplevel):
         GButton_290["justify"] = "center"
         GButton_290["text"] = "Cancelar"
         GButton_290.place(x=320,y=100,width=70,height=25)
-        GButton_290["command"] = self.GButton_290_command
+        GButton_290["command"] = self.cerrar
 
         GButton_722=tk.Button(self)
         GButton_722["bg"] = "#f0f0f0"
@@ -81,17 +80,32 @@ class Login(tk.Toplevel):
         GButton_722["justify"] = "center"
         GButton_722["text"] = "Usuario Nuevo"
         GButton_722.place(x=30,y=100,width=100,height=25)
-        GButton_722["command"] = self.GButton_722_command
+        GButton_722["command"] = self.abrir_user
 
-    def GButton_962_command(self):
-        print("command")
+    def iniciar_sesion(self):
+        try:
+            txtUsuario = self.nametowidget("txtUsuario")
+            usuario = txtUsuario.get()            
+
+            txtContrasenia = self.nametowidget("txtContrasenia")
+            contrasenia = txtContrasenia.get()
+
+            if usuario != "":
+                if user.validar(usuario, contrasenia):
+                    Dashboard(self.master)
+                    self.destroy()
+                else:
+                    tkMsgBox.showwarning(self.master.title(), "Usuario/Contraseña incorrecta")
+            else:
+                tkMsgBox.showwarning(self.master.title(), "Ingrese el Usuario para iniciar sesión")
+        except Exception as ex:
+            tkMsgBox.showerror(self.master.title(), str(ex))
+
+    def cerrar(self):
+        self.destroy()
 
 
-    def GButton_290_command(self):
-        print("command")
-
-
-    def GButton_722_command(self):
-        print("command")
+    def abrir_user(self):
+        User(self.master)
 
 
