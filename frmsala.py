7d63747c4b469e4665623a563590782f1329ca5e
alecobos <alecobos@gmail.com>
2022-12-07 22:40:31 -0300

    .

--- frmsala.py
+++ frmsala.py
@@ -3,20 +3,22 @@ import tkinter.ttk as ttk
 import tkinter.font as tkFont
 import tkinter.messagebox as tkMsgBox
 
-class App:
-    def __init__(self, root):
+class Sala(tk.Toplevel):
+    def __init__(self, master=None, isAdmin = False, id_sala = None):
+        super().__init__(master)
+        self.master = master    
         #setting title
-        root.title("undefined")
+        self.title("Sala")
         #setting window size
         width=280
         height=240
-        screenwidth = root.winfo_screenwidth()
-        screenheight = root.winfo_screenheight()
+        screenwidth = self.winfo_screenwidth()
+        screenheight = self.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        root.geometry(alignstr)
-        root.resizable(width=False, height=False)
+        self.geometry(alignstr)
+        self.resizable(width=False, height=False)
 
-        GLabel_696=tk.Label(root)
+        GLabel_696=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_696["font"] = ft
         GLabel_696["fg"] = "#333333"
@@ -24,7 +26,7 @@ class App:
         GLabel_696["text"] = "Nombre de la Sala:"
         GLabel_696.place(x=40,y=40,width=110,height=30)
 
-        GLineEdit_335=tk.Entry(root)
+        GLineEdit_335=tk.Entry(self)
         GLineEdit_335["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
         GLineEdit_335["font"] = ft
@@ -33,7 +35,7 @@ class App:
         GLineEdit_335["text"] = ""
         GLineEdit_335.place(x=160,y=40,width=80,height=30)
 
-        GLabel_455=tk.Label(root)
+        GLabel_455=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_455["font"] = ft
         GLabel_455["fg"] = "#333333"
@@ -41,7 +43,7 @@ class App:
         GLabel_455["text"] = "Capacidad:"
         GLabel_455.place(x=80,y=80,width=70,height=30)
 
-        GLineEdit_621=tk.Entry(root)
+        GLineEdit_621=tk.Entry(self)
         GLineEdit_621["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
         GLineEdit_621["font"] = ft
@@ -50,7 +52,7 @@ class App:
         GLineEdit_621["text"] = ""
         GLineEdit_621.place(x=160,y=80,width=80,height=30)
 
-        GLabel_829=tk.Label(root)
+        GLabel_829=tk.Label(self)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_829["font"] = ft
         GLabel_829["fg"] = "#333333"
@@ -58,10 +60,10 @@ class App:
         GLabel_829["text"] = "Tipo:"
         GLabel_829.place(x=95,y=120,width=70,height=30)
 
-        cb_roles = ttk.Combobox(root, state="readonly", values=["2D", "3D", "4D", "IMAX"])
+        cb_roles = ttk.Combobox(self, state="readonly", values=["2D", "3D", "4D", "IMAX"])
         cb_roles.place(x=160,y=120,width=80,height=30)
 
-        GButton_989=tk.Button(root)
+        GButton_989=tk.Button(self)
         GButton_989["bg"] = "#f0f0f0"
         ft = tkFont.Font(family='Times',size=10)
         GButton_989["font"] = ft
@@ -71,7 +73,7 @@ class App:
         GButton_989.place(x=90,y=190,width=70,height=25)
         GButton_989["command"] = self.GButton_989_command
 
-        GButton_820=tk.Button(root)
+        GButton_820=tk.Button(self)
         GButton_820["bg"] = "#f0f0f0"
         ft = tkFont.Font(family='Times',size=10)
         GButton_820["font"] = ft
@@ -88,7 +90,3 @@ class App:
     def GButton_820_command(self):
         print("command")
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = App(root)
-    root.mainloop()
